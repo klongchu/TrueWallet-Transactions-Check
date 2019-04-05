e659cee9e28b96e829c95569a0e0a5df901d847a
MrTheBank <thebankzana@yahoo.com>
2019-04-05 13:59:46 +0700

    อัพโหลดไฟล์เข้าสู่ GitHub

---
+++ inc/config.php
@@ -0,0 +1,31 @@
+<?php
+
+//ให้ใส่ E-Mail ของ Truemoney Wallet
+$twemail = "TrueWallet E-Mail";
+//ให้ใส่รหัสผ่าน E-Mail ของ Truemoney Wallet
+$twpassword = "TrueWallet Password";
+
+//Database ส่วนของการเชื่อมต่อฐานข้อมูล
+
+//Host ของฐานข้อมูล
+$dbhost = "Your hostname/IP";
+//Username ของฐานข้อมูล
+$dbuser = "Your database username";
+//Password ของฐานข้อมูล
+$dbpassword = "Your database password";
+//ฐานข้อมูล
+$dbname = "Your database name";
+
+//Database ส่วนของตารางในการเพิ่มพ้อยให้กับลูกค้า
+
+//ตารางของพ้อยที่ถูกเก็บ
+$tbname = "";
+//Field ที่ใช้อ้างอิง Username
+$field_username = "";
+//Field ที่ใช้ในการเก็บพ้อยจากการเติมเงิน
+$point_field_name = "";
+
+//จำนวน เงินเติมคูณด้วยเลขที่ต้องการ (เช่น เงินเติม 50 บาท x 1 = 50 บาท)
+$wallet_x = "1";
+
+?>
---
+++ inc/wallet.php
@@ -0,0 +1,81 @@
+<?php
+use Maythiwat\WalletAPI;
+require_once(__DIR__ . '/WalletAPI.php');
+include 'config.php';
+$tw = new WalletAPI();
+
+$ref = $_POST['wallet'];
+$member = $_POST['member'];
+// Login
+$token = $tw->GetToken($twemail, $twpassword);
+
+//Connect to database
+$mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
+
+  $strSQL2 = "SELECT * FROM tw_transactions WHERE txn_id = '". $ref ."'";
+  $objQuery2 = $mysqli->query($strSQL2);
+  $objResult2 = $objQuery2->fetch_assoc();
+
+
+// If successfully login
+if ($token != null) {
+  
+  // Transaction date range
+  $start_date = date('Y-m-d', strtotime('-1 days'));
+  $end_date = date('Y-m-d', strtotime('1 days'));
+  
+  // Perform Fetch
+  $activities = $tw->FetchActivities($token, $start_date, $end_date);
+  
+  foreach($activities as $arr) {
+    // Check is paid-in
+    if ($arr['original_action'] == 'creditor') {
+      $data = $tw->FetchTxDetail($token, $arr['report_id']);
+      
+      // Transaction ID
+      $tx['id'] = $data['section4']['column2']['cell1']['value'];
+
+      // Amount
+      $tx['amount'] = str_replace(',', '', $data['section3']['column1']['cell1']['value']);
+
+      $amount = $tx['amount'];
+
+      //Add TrueWallet transactions history to database to prevent abuse of transaction number.
+      $tw_history = "INSERT INTO tw_transactions (name, txn_id) VALUES ('". $member ."', '". $ref ."')";
+      //Point multiply
+      $dbpoint = $amount*$wallet_x;
+      //Add point to player once $objResult2 is checked.
+      $addpoint = "UPDATE ". $tbname ." SET ". $point_field_name ." = ". $point_field_name ."+'". $dbpoint ."' WHERE ". $field_username ." = ". $member ." ";
+      
+      // Then you can check user input and connect to database.
+      if ($objResult2) {
+        echo "<script language=\"JavaScript\">
+                alert('ล้มเหลว เลขอ้างอิง $ref ถูกใช้งานไปแล้ว');
+                window.history.go(-1);
+              </script>";
+        break;
+      } elseif ($ref === $tx['id']) {
+        echo "<script language=\"JavaScript\">
+                alert('ได้รับเงินจำนวน $amount บาท และพ้อยเป็นจำนวน $dbpoint พ้อย');
+                window.history.go(-1);
+              </script>";
+        //Database เพิ่มประวัติการใช้เลขอ้างอิงเพื่อไม่ให้ใช้เลขอ้างอิงซ้ำอีกรอบ
+        mysqli_query($mysqli, $tw_history);
+        //Database เพิ่มพ้อยให้กับผู้เล่น
+        mysqli_query($mysqli, $addpoint);
+        break;
+      } else {
+        echo "<script language=\"JavaScript\">
+                alert('ผิดพลาด เลข $ref ดังกล่าวไม่ถูกต้อง');
+                window.history.go(-1);
+              </script>";
+        break;
+      }
+    }
+  }
+  
+  // Logout
+  $tw->Logout($token);
+  mysql_close($mysqli);
+}
+?>
