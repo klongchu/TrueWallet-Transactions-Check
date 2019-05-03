c5aafefbf4a5e031d1aec1c6bf9c36c3587c3d81
MrTheBank <thebankzana@yahoo.com>
2019-05-03 13:58:46 +0700

    Create choose.php

---
+++ choose.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TrueWallet - TrueMoney by MrTheBank</title>
+	<meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+<div class="w3-display-middle">
+  <h1>ฉันอยากจะเติมเงินเข้าสู่ระบบด้วย</h1>
+  <center>
+  <button type="button" class="btn btn-primary btn-lg btn-block" onclick="window.location.href='index.php?page=truewallet'">TrueWallet</button><BR><BR>
+  <button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='index.php?page=truemoney'">TrueMoney</button>
+  </center>
+</div>
+</body>
+</html>
